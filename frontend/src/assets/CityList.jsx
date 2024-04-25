6fbad66a5fe525acfba983f5849d22376a96eb14
EPedley <lizzypedley@gmail.com>
2024-04-25 17:17:54 +0100

    Adding filtering by country and component

---
+++ frontend/src/assets/CityList.jsx
@@ -0,0 +1,7 @@
+const cities = [
+    'New York', 'Tokyo', 'London', 'Paris', 'Beijing', 'Sydney', 'Mumbai', 'Moscow',
+    'Rio', 'Cairo', 'Istanbul', 'Toronto', 'Cape Town', 'Mexico City', 'Dubai',
+    'Berlin', 'Buenos Aires', 'Seoul', 'Rome', 'Bangkok'
+];
+
+export default cities;
