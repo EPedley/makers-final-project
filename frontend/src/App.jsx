194157b51e11add7220d4db47978029e39400b9d
EPedley <lizzypedley@gmail.com>
2024-04-29 17:05:23 +0100

    removing old about page & routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { MainPage } from "./pages/MainPage";
-import { AboutPage } from "./pages/AboutPage";
 
 // docs: https://reactrouter.com/en/main/start/overview
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainPage />,
-  },
-  {
-    path: "/about",
-    element: <AboutPage />,
   }
 ]);
 
