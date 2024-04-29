b1441715087511d70cab18c7b968d0f7387c8480
EPedley <lizzypedley@gmail.com>
2024-04-29 10:10:16 +0100

    moving colour map into /data

---
+++ frontend/src/data/ColourMap.jsx
@@ -0,0 +1,22 @@
+const colourMap = {
+    // teal
+    "aqi": { min: "#ccdedd", max: "#339A8C" },
+    // pink
+    "co": { min: "#F1CAD6", max: "#AA4465" },
+    // blue
+    "no": { min: "#CCDFFC", max: "#1B4079" },
+    // purple
+    "no2": { min: "#F9DCFF", max: "#835091" },
+    // fushcia
+    "o3": { min: "#FFC9DF", max: "#FF0066" },
+    // orange
+    "so2": { min: "#EBD0C0", max: "#E1671C" },
+    // light blue
+    "pm2_5": { min: "#C6F0F0", max: "#28AFB0" },
+    // yellow
+    "pm10": { min: "#FFE2BC", max: "#FFA630" },
+    // purple
+    "nh3": { min: "#d8d1e1", max: "#63458A" }
+};
+
+export default colourMap;
