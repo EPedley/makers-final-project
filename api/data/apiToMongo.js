80b9cef92ee8033fee1d3672fbd764f21b4d2179
Ledia <ledialuli@icloud.com>
2024-05-01 15:31:03 +0100

    files

--- api/data/apiToMongo.js
+++ api/data/apiToMongo.js
@@ -2,84 +2,95 @@ const { MongoClient } = require('mongodb');
 const apiKey = require('../../apiKey');
 const countries = require("./capitalCitiesList");
 const mongoose = require('mongoose');
-const City = require("../models/City") // Assuming User model is defined in a separate file
+const City = require("../models/City");
 const { connectToDatabase } = require("../db/db");
-const { sub } = require('date-fns');
+const { subDays, format } = require('date-fns');
 require("dotenv").config();
 
-
 // Function to fetch data from API
-async function fetchDataFromAPI(lat, lon) {
-    const apiKeytoUse = apiKey
-    const apiURL = `http://api.openweathermap.org/data/2.5/air_pollution/history?lat=${lat}&lon=${lon}&start=1711969200&end=1714302000&appid=${apiKeytoUse}`;
-        try {
-            const response = await fetch(apiURL, {
-                headers: {
-                    'Authorization': `Bearer ${apiKeytoUse}`,
-                    'Content-Type': 'application/json'
-                }
-            });
-
-            if (!response.ok) {
-                throw new Error(`Failed to fetch data from API: ${response.statusText}`);
+async function fetchDataFromAPI(lat, lon, start, end) {
+    const apiKeytoUse = apiKey;
+    const apiURL = `http://api.openweathermap.org/data/2.5/air_pollution/history?lat=${lat}&lon=${lon}&start=${start}&end=${end}&appid=${apiKeytoUse}`;
+    try {
+        const response = await fetch(apiURL, {
+            headers: {
+                'Authorization': `Bearer ${apiKeytoUse}`,
+                'Content-Type': 'application/json'
             }
+        });
 
-            const data = await response.json();
-            return data;
-        } catch (error) {
-            console.error('Error fetching data from API:', error);
-            throw error; 
+        if (!response.ok) {
+            throw new Error(`Failed to fetch data from API: ${response.statusText}`);
         }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching data from API:', error);
+        throw error;
+    }
 }
 
-const seedDatabase = async (data) => {
+// Function to seed database with fetched data
+async function seedDatabase(data) {
     try {
         await connectToDatabase();
-        const createNewCity = async () => {       
+        const createNewCity = async () => {
             for (const item of data.list) {
-                const city = countries.find(entry => entry.Lat === data.coord.lat).City
+                const city = countries.find(entry => entry.Lat === data.coord.lat).City;
                 const date = new Date(item.dt * 1000);
                 const hour = date.getHours();
-                var response = ""
-                // TODO: CALCULATE FOR TIMEZONE
-                if (hour === 13) {  
-                    response = await City.create({location: city, aqi: item.main.aqi , date: date, co: item.components.co, no: item.components.no, no2: item.components.no2, o3: item.components.o3, so2: item.components.so2, pm2_5: item.components.pm2_5, pm10: item.components.pm10, nh3: item.components.nh3})
-                    console.log('Cities inserted successfully' + response);
+                // Calculate start and end timestamps for last two days
+                const end = Math.floor(Date.now() / 1000);
+                const start = Math.floor(subDays(end, 2) / 1000);
+                if (hour === 13) {
+                    await City.create({ location: city, aqi: item.main.aqi, date: date, co: item.components.co, no: item.components.no, no2: item.components.no2, o3: item.components.o3, so2: item.components.so2, pm2_5: item.components.pm2_5, pm10: item.components.pm10, nh3: item.components.nh3 });
+                    console.log('City inserted successfully');
                 }
-                
             }
-        }
+        };
 
-        await createNewCity()
-        console.log("City created successfully")
+        await createNewCity();
+        console.log("Cities created successfully");
 
     } catch (error) {
         console.error('Error seeding:', error);
-    } 
-    // finally {
-    //     // Close the database connection
-    //     await mongoose.connection.close(true);
-    // }
+    } finally {
+        // Close the database connection
+        await mongoose.connection.close(true);
+    }
 }
 
 async function main() {
     try {
-        for (let i = 0; i < countries.length; i++) {
-            console.log("RUNNING " + i)
-            const lat = countries[i].Lat
-            const lon = countries[i].Lon
-            const data = await fetchDataFromAPI(lat, lon);
-            await seedDatabase(data);
-    }
+        // Connect to the database
+        await connectToDatabase();
+
+        // Query the database to get the timestamp of the last entry
+        const lastEntry = await City.findOne({}, {}, { sort: { 'date': -1 } });
+
+        // Calculate start and end timestamps for last two days
+        const end = Math.floor(Date.now() / 1000);
+        const start = Math.floor(subDays(end, 2) / 1000);
+
+        // If the difference is more than two days, fetch data from the API and seed the database
+        if (lastEntry && lastEntry.date < start) {
+            for (let i = 0; i < countries.length; i++) {
+                console.log("RUNNING " + i);
+                const lat = countries[i].Lat;
+                const lon = countries[i].Lon;
+                const data = await fetchDataFromAPI(lat, lon, start, end);
+                await seedDatabase(data);
+            }
+        } else {
+            console.log("Data is up to date, no need to fetch from API.");
+        }
     } catch (error) {
-            console.error('An error occurred:', error);
+        console.error('An error occurred:', error);
     } finally {
-        // Close the database connection when all operations are finished
+        // Close the database connection
         await mongoose.connection.close(true);
     }
 }
-    
-    
-main()
 
-
+main();
--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,27 +1,27 @@
 import { useState, useEffect } from "react";
 
-// functions
-import loadDataFromMongoDB from "../services/requests"
+// Functions
+import loadDataFromMongoDB from "../services/requests";
+import { apiToMongo } from "./"; // Import the function from your utility file
 
-// assets
+// Assets
 import terraFullLogo from "../logos/terraFullLogo.png";
-import "./MainPage.css"
+import "./MainPage.css";
 
-// components
+// Components
 import { About } from "../components/About";
 import { Map } from "../components/Map";
 import { InformationButton } from "../components/InformationButton";
-import ChartComponent from "../components/TableV2"
+import ChartComponent from "../components/TableV2";
 
-// data
+// Data
 import components from "../data/ComponentList";
-
 
 export const MainPage = () => {
     const [data, setData] = useState([]);
     const [componentFilter, setComponentFilter] = useState("aqi");
     const [countryFilter, setCountryFilter] = useState("");
-    const date = new Date(Date.now() - 864e5)
+    const date = new Date(Date.now() - 864e5);
     const [aboutVisible, setAboutVisible] = useState(false);
 
     const handleComponentFilterChange = (event) => {
@@ -32,78 +32,79 @@ export const MainPage = () => {
     const handleCountryFilterChange = (city) => {
       setCountryFilter(city);
     };
-  
+
     useEffect(() => {
+        // Load data from MongoDB when component mounts
         loadDataFromMongoDB()
-        .then(data => {
-            setData(data)
-        })
-        .catch(error => {
-            console.error('An error occurred:', error);
-            setData([])
-        });
+            .then(data => {
+                setData(data);
+            })
+            .catch(error => {
+                console.error('An error occurred:', error);
+                setData([]);
+            });
+
+        // Check last data entry timestamp and fetch data if it's over two days old
+        fetchDataAndUpdateDB();
     }, []);
 
     const formatDate = (dateString) => {
-      const options = {month: 'long', day: 'numeric', year: 'numeric'}
+      const options = {month: 'long', day: 'numeric', year: 'numeric'};
       const date = new Date(dateString);
-      return date.toLocaleDateString('en-US', options)
-    }
+      return date.toLocaleDateString('en-US', options);
+    };
 
     const toggleAbout = () => {
       setAboutVisible(!aboutVisible);
     };
-  
 
-  return (
-    <div className="mainPage">
+    return (
+        <div className="mainPage">
+            <div className="logo">
+                <img src={terraFullLogo} alt="Terra Logo" />
+            </div>
+                
+            <button className="aboutButton" onClick={toggleAbout}></button>
 
-      <div className="logo">
-        <img src={terraFullLogo} alt="Terra Logo" />
-      </div>
-          
-      <button className="aboutButton" onClick={toggleAbout}></button>
+            {aboutVisible && (
+                <div className="aboutOverlay">
+                    <About/>
+                </div>
+            )}
 
-      {aboutVisible && (
-        <div className="aboutOverlay">
-          <About/>
+            <div className="select">
+                <select name="componentFilter" id="componentFilter" onChange={handleComponentFilterChange}>
+                    {components.map((component, index) => (
+                        <option key={index} value={component.value}>{component.label}</option>
+                    ))}
+                </select>
+                <div className="select_arrow"></div>
+            </div>
+
+            <Map 
+                handleCountryFilterChange={handleCountryFilterChange} 
+                componentFilter={componentFilter} 
+                countryFilter={countryFilter} 
+                data={data.data}
+                date={date}
+            />
+
+            <div className="mapLabel">
+                World mapchart showing 
+                {data && " " + components.find(component => component.value === componentFilter)?.label} 
+                <InformationButton componentFilter={componentFilter}/>
+                <span> </span>
+                {componentFilter != "aqi" && "concentration in μg/m³ "}
+                for {formatDate(date)} from <a href="https://openweathermap.org/">Open Weather</a>
+            </div>
+
+            {data.length === 0 ? (
+                <p>Loading...</p>
+            ) : (
+                <ChartComponent data={data.data} componentFilter={componentFilter} countryFilter={countryFilter} />
+            )}
         </div>
-      )}
-
-      <div className="select">
-        <select name="componentFilter" id="componentFilter" onChange={handleComponentFilterChange}>
-          {components.map((component, index) => (
-            <option key={index} value={component.value}>{component.label}</option>
-          ))}
-        </select>
-        <div className="select_arrow"></div>
-      </div>
-
-      <Map 
-        handleCountryFilterChange={handleCountryFilterChange} 
-        componentFilter={componentFilter} 
-        countryFilter={countryFilter} 
-        data={data.data}
-        date={date}
-      />
-
-      <div className = "mapLabel">
-        World mapchart showing 
-        {data && " " + components.find(component => component.value === componentFilter)?.label} 
-        <InformationButton componentFilter={componentFilter}/>
-        <span> </span>
-        {componentFilter != "aqi" && "concentration in μg/m³ "}
-        for {formatDate(date)} from <a href="https://openweathermap.org/">Open Weather</a>
-      </div>
-
-      {data.length === 0 ? (
-        <p>Loading...</p>
-      ) : (
-      <ChartComponent data={data.data} componentFilter={componentFilter} countryFilter={countryFilter} />
-      )}
-
-      </div>
     );
 };
 
-    
+export default MainPage;
